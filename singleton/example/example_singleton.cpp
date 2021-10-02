64d463ea4552e224c263c3dc512f18b58bee06c3
Anton Kesy <anton@kesy.de>
2021-10-02 16:59:38 +0200

    Update example_singleton.cpp

--- singleton/example/example_singleton.cpp
+++ singleton/example/example_singleton.cpp
@@ -1,6 +1,22 @@
-#include "example_singleton.h"
+#include "../singleton.h"
 
-int main(void)
-{
-    return 0;
+namespace design_pattern::singleton_design_pattern {
+
+    int example_usage()
+    {
+        design_pattern::singleton_design_pattern::Singleton& s1 = Singleton::getSingleton();
+        Singleton& s2 = Singleton::getSingleton();
+        if (&s1 != &s2)
+        {
+            return 1;
+        }
+        return 0;
+    }
+
 }
+
+int main()
+{
+    return design_pattern::singleton_design_pattern::example_usage();
+}
+
