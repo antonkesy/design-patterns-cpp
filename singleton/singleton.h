1c7c256d217e035665eb58a15a75e3af1f39f44c
Anton Kesy <anton@kesy.de>
2021-10-02 16:50:08 +0200

    use c++17 namespace

--- singleton/singleton.h
+++ singleton/singleton.h
@@ -1,4 +1,26 @@
+namespace design_pattern::singleton_design_pattern {
 #ifndef DESIGN_PATTERN_CPP_SINGLETON_H
 #define DESIGN_PATTERN_CPP_SINGLETON_H
 
-#endif //DESIGN_PATTERN_CPP_SINGLETON_H
+    class Singleton
+    {
+    public:
+        static Singleton& getSingleton()
+        {
+            //lazy initialization
+            static Singleton INSTANCE;
+            return INSTANCE;
+        }
+
+        ~Singleton() = default;
+
+        Singleton(Singleton const&) = delete;
+
+        void operator=(Singleton const&) = delete;
+
+    private:
+        Singleton() = default;
+    };
+
+#endif
+}
