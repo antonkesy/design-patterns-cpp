6260597d17f9221533152d0cbd887b5457958fd5
Anton Kesy <anton@kesy.de>
2022-06-07 11:00:28 +0200

    refactor mediator

--- mediator/mediator.h
+++ mediator/mediator.h
@@ -1,18 +1,17 @@
-#include <iostream>
-#include <vector>
-
-namespace design_pattern::mediator
-{
 #ifndef DESIGN_PATTERN_CPP_MEDIATOR_H
 #define DESIGN_PATTERN_CPP_MEDIATOR_H
 
+#include <iostream>
+#include <vector>
+#include <memory>
+
+namespace design_pattern::mediator {
+
     class Mediator;
 
-    class Colleague
-    {
+    class Colleague {
     public:
-        Colleague(Mediator* mediator, std::size_t id) : mediator(mediator), id(id)
-        {}
+        Colleague(Mediator &mediator, std::size_t id) : mediator(mediator), id(id) {}
 
         virtual ~Colleague() = default;
 
@@ -20,81 +19,62 @@ namespace design_pattern::mediator
 
         virtual void Send(int number) = 0;
 
-        std::size_t GetID() const
-        {
+        [[nodiscard]] std::size_t GetID() const {
             return id;
         }
 
     protected:
-        Mediator* mediator;
+        Mediator &mediator;
         std::size_t id;
     };
 
-    class Mediator
-    {
+    class Mediator {
     public:
         virtual ~Mediator() = default;
 
-        virtual void Notify(Colleague* colleague, int number) = 0;
+        virtual void Notify(Colleague &colleague, int number) = 0;
 
     };
 
 
-    class ConcreteColleague : public Colleague
-    {
+    class ConcreteColleague : public Colleague {
     public:
-        ConcreteColleague(Mediator* mediator, unsigned int id) : Colleague(mediator, id)
-        {}
+        ConcreteColleague(Mediator &mediator, unsigned int id) : Colleague(mediator, id) {}
 
         ~ConcreteColleague() override = default;
 
-        void Receive(int number) override
-        {
-            std::cout << "colleague id=" << id << " received number=" << number << std::endl;
+        void Receive(int number) override {
+            std::cout << "colleague id=" << id << " received number=" << number << "\n";
         }
 
-        void Send(int number) override
-        {
-            std::cout << "colleague id=" << id << " send number=" << number << std::endl;
-            mediator->Notify(this, number);
+        void Send(int number) override {
+            std::cout << "colleague id=" << id << " send number=" << number << "\n";
+            mediator.Notify(*this, number);
         }
     };
 
-    class ConcreteMediator : public Mediator
-    {
+    class ConcreteMediator : public Mediator {
     public:
         ConcreteMediator() = default;
 
-        ~ConcreteMediator() override
-        {
-            for (const auto& item: _colleagues)
-            {
-                delete item;
-            }
-        }
+        ~ConcreteMediator() override = default;
 
-        void Add(Colleague* colleague)
-        {
+        void Add(const std::shared_ptr<Colleague>& colleague) {
             _colleagues.push_back(colleague);
         }
 
-        void Notify(Colleague* sender, int number) override
-        {
-            for (const auto& item: _colleagues)
-            {
+        void Notify(Colleague &sender, int number) override {
+            for (const auto &item: _colleagues) {
                 //distribute to all except to sender
-                if (item->GetID() != sender->GetID())
-                {
+                if (item->GetID() != sender.GetID()) {
                     item->Receive(number);
                 }
             }
         }
 
-
     private:
-        std::vector<Colleague*> _colleagues;
+        std::vector<std::shared_ptr<Colleague>> _colleagues;
     };
 
-
-#endif
 }
+#endif
