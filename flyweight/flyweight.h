5d7867c79c08db8e46c73e155caaf7a0732327f2
Anton Kesy <anton@kesy.de>
2022-06-07 10:32:54 +0200

    refactor flyweight

--- flyweight/flyweight.h
+++ flyweight/flyweight.h
@@ -1,13 +1,13 @@
+#ifndef DESIGN_PATTERN_CPP_FLYWEIGHT_H
+#define DESIGN_PATTERN_CPP_FLYWEIGHT_H
+
 #include <map>
 #include <iostream>
+#include <memory>
 
-namespace design_pattern::flyweight
-{
-#ifndef DESIGN_PATTERN_CPP_FLYWEIGHT_H
-#define DESIGN_PATTERN_CPP_SINGLETON_H
+namespace design_pattern::flyweight {
 
-    class Flyweight
-    {
+    class Flyweight {
     public:
         virtual ~Flyweight() = default;
 
@@ -15,72 +15,56 @@ namespace design_pattern::flyweight
     };
 
     //implements Flyweight interface and adds storage for intrinsic state
-    class ConcreteFlyweight : public Flyweight
-    {
+    class ConcreteFlyweight : public Flyweight {
     public:
-        explicit ConcreteFlyweight(int intrinsic_state) : _intrinsic_state(intrinsic_state)
-        {
-            std::cout << "Create ConcreteFlyweight with state " << _intrinsic_state << std::endl;
+        explicit ConcreteFlyweight(int intrinsic_state) : intrinsic_state_(intrinsic_state) {
+            std::cout << "Create ConcreteFlyweight with state " << intrinsic_state_ << "\n";
         }
 
         ~ConcreteFlyweight() override = default;
 
-        void Operation() override
-        {
-            std::cout << "ConcreteFlyweight with state " << _intrinsic_state << std::endl;
+        void Operation() override {
+            std::cout << "ConcreteFlyweight with state " << intrinsic_state_ << "\n";
         }
 
     private:
-        int _intrinsic_state;
+        int intrinsic_state_;
     };
 
     //not all subclasses need to be shared
-    class UnsharedConcreteFlyweight : public Flyweight
-    {
+    class UnsharedConcreteFlyweight : public Flyweight {
     public:
-        explicit UnsharedConcreteFlyweight(int state) : _state(state)
-        {}
+        explicit UnsharedConcreteFlyweight(int state) : state_(state) {}
 
         ~UnsharedConcreteFlyweight() override = default;
 
-        void Operation() override
-        {
-            std::cout << "Unshared Flyweight with state " << _state << std::endl;
+        void Operation() override {
+            std::cout << "Unshared Flyweight with state " << state_ << "\n";
         }
 
     private:
-        int _state;
+        int state_;
     };
 
     //creates and manages flyweights
-    class FlyweightFactory
-    {
+    class FlyweightFactory {
     public:
-        ~FlyweightFactory()
-        {
-            //delete every flyweight in pool
-            for (const auto& flyweight: _flyweights)
-            {
-                delete flyweight.second;
-            }
-        }
+        ~FlyweightFactory() = default;
 
-        Flyweight* GetFlyweight(int key)
-        {
+        std::shared_ptr<Flyweight> GetFlyweight(int key) {
             //check if flyweight is already in pool;
-            if (_flyweights.find(key) != _flyweights.end())
-            {
+            if (_flyweights.find(key) != _flyweights.end()) {
                 return _flyweights[key];
             }
             //create new flyweight if not in pool already
-            Flyweight* new_flyweight = new ConcreteFlyweight(key);
-            _flyweights.insert(std::pair<int, Flyweight*>(key, new_flyweight));
+            auto new_flyweight = std::make_shared<ConcreteFlyweight>(key);
+            _flyweights.insert(std::pair<int, std::shared_ptr<Flyweight> >(key, new_flyweight));
             return new_flyweight;
         }
 
     private:
-        std::map<int, Flyweight*> _flyweights;
+        std::map<int, std::shared_ptr<Flyweight> > _flyweights;
     };
 
-#endif
 }
+#endif
