9d40405553c36191d8fa559e07573fdb05b43b94
Anton Kesy <anton@kesy.de>
2022-06-07 11:49:15 +0200

    refactor state

--- state/state.h
+++ state/state.h
@@ -1,66 +1,55 @@
-#include <iostream>
-#include <list>
-
-namespace design_pattern::state
-{
 #ifndef DESIGN_PATTERN_CPP_STATE_H
 #define DESIGN_PATTERN_CPP_STATE_H
 
-    class State
-    {
+#include <iostream>
+#include <list>
+#include <memory>
+
+namespace design_pattern::state {
+
+    class State {
     public:
         virtual ~State() = default;
 
         virtual void Handle() = 0;
     };
 
-    class ConcreteState1 : public State
-    {
+    class ConcreteState1 : public State {
     public:
         ~ConcreteState1() override = default;
 
-        void Handle() override
-        {
-            std::cout << "concrete state 1" << std::endl;
+        void Handle() override {
+            std::cout << "concrete state 1\n";
         }
     };
 
-    class ConcreteState2 : public State
-    {
+    class ConcreteState2 : public State {
     public:
         ~ConcreteState2() override = default;
 
-        void Handle() override
-        {
-            std::cout << "concrete state 2" << std::endl;
+        void Handle() override {
+            std::cout << "concrete state 2\n";
         }
     };
 
-    class Context
-    {
+    class Context {
     public:
         explicit Context() = default;
 
-        ~Context()
-        {
-            delete _state;
-        }
+        ~Context() = default;
 
-        void Request()
-        {
+        void Request() {
             if (_state)
                 _state->Handle();
         }
 
-        void SetState(State* state)
-        {
-            delete _state;
-            _state = state;
+        void SetState(std::unique_ptr<State> state) {
+            _state = std::move(state);
         }
 
     private:
-        State* _state = nullptr;
+        std::unique_ptr<State> _state = nullptr;
     };
 
-#endif
 }
+#endif
