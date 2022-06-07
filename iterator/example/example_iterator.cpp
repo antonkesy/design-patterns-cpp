e4f03be4192b38e83b5134e50ea6f28f973724b0
Anton Kesy <anton@kesy.de>
2022-06-07 10:50:53 +0200

    refactor iterator

--- iterator/example/example_iterator.cpp
+++ iterator/example/example_iterator.cpp
@@ -2,61 +2,21 @@
 #include <iostream>
 #include "../iterator.h"
 
-#define TEST_LIST_SIZE 5
+int main() {
+    using design_pattern::iterator::ConcreteAggregate;
+    using design_pattern::iterator::Iterator;
 
-namespace design_pattern::iterator
-{
+    const int test_list_size = 5;
+    ConcreteAggregate<int> list(test_list_size);
 
-    template<class T>
-    void populate_aggregate(ConcreteAggregate<T>& aggregate)
-    {
-        for (int i = 0; i < TEST_LIST_SIZE; ++i)
-        {
-            aggregate[i] = i;
-        }
+    for (int i = 0; i < test_list_size; ++i) {
+        list[i] = i;
     }
 
-    int example_usage()
-    {
-        ConcreteAggregate<int> list(TEST_LIST_SIZE);
-        populate_aggregate(list);
-
-        Iterator<int>* it = list.CreateIterator();
-        for (it->First(); !it->IsDone(); it->Next())
-        {
-            std::cout << it->CurrentItem() << std::endl;
-        }
-
-        delete it;
-        return 0;
+    auto it = list.CreateIterator();
+    for (it->First(); !it->IsDone(); it->Next()) {
+        std::cout << it->CurrentItem() << "\n";
     }
 
-    int example_usage_std()
-    {
-        std::list<int> list;
-        //populate list
-        for (int i = 0; i < 10; ++i)
-        {
-            list.push_back(i);
-        }
-
-        //for ease of understanding simple std example (no auto)
-        std::list<int>::iterator iterator = list.begin();
-        while (iterator++ != list.end())
-        {
-            std::cout << *iterator << std::endl;
-        }
-
-
-        return 0;
-    }
-
+    return 0;
 }
-
-int main()
-{
-    design_pattern::iterator::example_usage_std();
-    return design_pattern::iterator::example_usage();
-}
-
-
