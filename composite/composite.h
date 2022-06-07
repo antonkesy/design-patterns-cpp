ef24b980983801f966dfdf958cd3e6a0cc2930a3
Anton Kesy <anton@kesy.de>
2022-06-07 10:00:54 +0200

    refactor composite

--- composite/composite.h
+++ composite/composite.h
@@ -1,73 +1,73 @@
+#ifndef DESIGN_PATTERN_CPP_COMPOSITE_H
+#define DESIGN_PATTERN_CPP_COMPOSITE_H
+
 #include <iostream>
 #include <utility>
 #include <vector>
 #include <memory>
 
 namespace design_pattern::composite {
-#ifndef DESIGN_PATTERN_CPP_COMPOSITE_H
-#define DESIGN_PATTERN_CPP_COMPOSITE_H
 
     class Component {
     public:
-        ~Component() = default;
+        virtual ~Component() = default;
 
         virtual void Operation() = 0;
 
-        virtual void Add(Component *new_child) { throw std::invalid_argument(""); };
+        virtual void Add(std::shared_ptr<Component> new_child) { throw std::invalid_argument(""); };
 
-        virtual void Remove(Component *child_to_remove) { throw std::invalid_argument(""); };
+        virtual void Remove(std::shared_ptr<Component> child_to_remove) { throw std::invalid_argument(""); };
 
-        virtual Component *GetChild(int index) { throw std::invalid_argument(""); };
+        virtual std::shared_ptr<Component> GetChild(int index) { throw std::invalid_argument(""); };
 
     };
 
     class Composite : public Component {
     public:
 
-        virtual  ~Composite() = default;
+        ~Composite() override = default;
 
         void Operation() override {
-            for (const auto &item: _children) {
+            for (const auto &item: children_) {
                 item->Operation();
             }
         }
 
-        void Add(Component *new_child) override {
-            _children.push_back(new_child);
+        void Add(std::shared_ptr<Component> new_child) override {
+            children_.push_back(new_child);
         }
 
-        void Remove(Component *child_to_remove) override {
-            //iterate over vector until found child_to_remove and then removes
-            for (auto it = std::begin(_children); it != std::end(_children); ++it) {
+        void Remove(std::shared_ptr<Component> child_to_remove) override {
+            for (auto it = std::begin(children_); it != std::end(children_); ++it) {
                 if (*it == child_to_remove) {
-                    _children.erase(it);
-                    break;
+                    children_.erase(it);
+                    return;
                 }
             }
         }
 
-        Component *GetChild(int index) override {
-            return _children.at(index);
+        std::shared_ptr<Component> GetChild(int index) override {
+            return children_.at(index);
         }
 
     private:
-        std::vector<Component *> _children;
+        std::vector<std::shared_ptr<Component> > children_;
     };
 
     class Leaf : public Component {
     public:
 
-        explicit Leaf(std::string message) : _message(std::move(message)) {}
+        explicit Leaf(std::string message) : message_(std::move(message)) {}
 
-        virtual  ~Leaf() = default;
+        ~Leaf() override = default;
 
         void Operation() override {
-            std::cout << _message << std::endl;
+            std::cout << message_ << "\n";
         }
 
     private:
-        std::string _message;
+        std::string message_;
     };
 
-#endif
 }
+#endif
